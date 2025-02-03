6daf113322236610541b5bcf9c3d3bad0dce7483
Arlindo Boa <arlindojosboa@gmail.com>
2025-02-03 02:06:48 +0200

    Update Sc

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -15,6 +15,6 @@ export const auth = defineAuth({
   },
   access: (allow) => [
     allow.resource(addUserToGroup).to(["addUserToGroup"]),
-    allow.resource(adminCreateUser).to(["createUser", "getUser"]),
+    allow.resource(adminCreateUser).to(["createUser"]),
   ],
 });
