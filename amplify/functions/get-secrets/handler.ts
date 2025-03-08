9567f2d0595866fdee90bfeba706d6a780c588fc
Arlindo Boa <arlindojosboa@gmail.com>
2025-03-08 12:42:26 +0200

    Fix Secrets function

--- amplify/functions/get-secrets/handler.ts
+++ amplify/functions/get-secrets/handler.ts
@@ -1,4 +1,4 @@
-import { env } from '$amplify/env/create-stream-token';
+import { env } from '$amplify/env/get-secrets';
 import { Schema } from '../../data/resource';
 
 type Handler = Schema['getSecrets']['functionHandler'];
@@ -7,9 +7,9 @@ export const handler: Handler = async (event) => {
   try {
     return {
       content: JSON.stringify({
-        awsAccessKeyId: env.AWS_ACCESS_KEY_ID,
-        awsSecretAccessKey: env.AWS_SECRET_ACCESS_KEY,
-        awsRegion: env.AWS_REGION
+        awsAccessKeyId: env.AMAZON_ACCESS_KEY_ID,
+        awsSecretAccessKey: env.AMAZON_SECRET_ACCESS_KEY,
+        awsRegion: env.AMAZON_REGION
       })
     };
   } catch (e) {
--- amplify/functions/get-secrets/resource.ts
+++ amplify/functions/get-secrets/resource.ts
@@ -1,8 +1,11 @@
-import { defineFunction } from "@aws-amplify/backend"
+import { defineFunction, secret } from "@aws-amplify/backend"
 
 export const getSecrets = defineFunction({
   name: "get-secrets",
   entry: './handler.ts',
   environment: {
+    AMAZON_ACCESS_KEY_ID: secret('AMAZON_ACCESS_KEY_ID'),
+    AMAZON_SECRET_ACCESS_KEY: secret('AMAZON_SECRET_ACCESS_KEY'),
+    AMAZON_REGION: secret('AMAZON_REGION')
   }
 })
