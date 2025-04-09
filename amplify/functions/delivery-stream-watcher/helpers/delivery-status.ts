fdeed39fea7f85cf58f3281050f25bed19e798eb
Arlindo Boa <arlindojosboa@gmail.com>
2025-04-09 11:16:26 +0200

    Update delivery status and worrkflow

--- amplify/functions/delivery-stream-watcher/helpers/delivery-status.ts
+++ amplify/functions/delivery-stream-watcher/helpers/delivery-status.ts
@@ -1,6 +1,7 @@
 import { DeliveryStatus } from "../../helpers/types/schema";
 
 const DELIVERY_STATUS_DESCRIPTIONS = new Map<DeliveryStatus, string>([
+  [DeliveryStatus.PENDING, 'Pendente'],
   [DeliveryStatus.PHARMACY_PREPARING, 'Preparando para entrega'],
   [DeliveryStatus.AWAITING_DRIVER_ASSIGNMENT, 'Aguardando motorista'],
   [DeliveryStatus.DRIVER_ASSIGNED, 'Motorista adicionado'],
