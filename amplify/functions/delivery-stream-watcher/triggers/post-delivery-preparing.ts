64267c657e5f86545fa568e1998085d7c46ce694
Arlindo Boa <arlindojosboa@gmail.com>
2025-04-09 16:34:55 +0200

    Create Post delivery preparing status

---
+++ amplify/functions/delivery-stream-watcher/triggers/post-delivery-preparing.ts
@@ -0,0 +1,28 @@
+import { Logger } from "@aws-lambda-powertools/logger";
+import type { AttributeValue } from "aws-lambda";
+import { DeliveryStatus } from "../../helpers/types/schema";
+import { createDeliveryStatusHistory } from "../helpers/create-delivery-status-history";
+
+interface TriggerInput {
+  deliveryImage: { [key: string]: AttributeValue; };
+  dbClient: any;
+  logger: Logger;
+}
+
+export const postDeliveryPreparing = async ({ deliveryImage, dbClient, logger }: TriggerInput) => {
+  const orderId = deliveryImage?.orderId?.S;
+  const patientId = deliveryImage?.patientId?.S;
+
+  if (!orderId || !patientId) {
+    logger.warn("Missing required order fields");
+    return;
+  }
+
+  await createDeliveryStatusHistory({
+    client: dbClient,
+    logger,
+    patientId: patientId,
+    deliveryId: orderId,
+    status: DeliveryStatus.PHARMACY_PREPARING
+  })
+};
