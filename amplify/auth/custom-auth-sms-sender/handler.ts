f8920c7386c6f613e6b56440edf0e04854f876cc
Arlindo Boa <arlindojosboa@gmail.com>
2025-05-09 15:29:25 +0200

    Created Auth SMS trigger Lambda

--- amplify/auth/custom-auth-sms-sender/handler.ts
+++ amplify/auth/custom-auth-sms-sender/handler.ts
@@ -1,7 +1,6 @@
 import { env } from '$amplify/env/custom-auth-sms-sender';
-import { DecryptCommand, KMSClient } from "@aws-sdk/client-kms";
+import { KMSClient } from "@aws-sdk/client-kms";
 import { CustomSMSSenderTriggerHandler } from 'aws-lambda';
-import { Buffer } from 'buffer';
 import { SendSMSService } from '../../functions/helpers/sendSms';
 
 const kmsClient = new KMSClient({ region: env.AWS_REGION });
@@ -10,27 +9,6 @@ const smsService = new SendSMSService({
   apiToken: env.SMS_API_KEY,
   senderId: env.SMS_SENDER_ID,
 });
-
-interface CustomSMSSenderEvent {
-  version: string;
-  triggerSource: string;
-  region: string;
-  userPoolId: string;
-  userName: string;
-  callerContext: {
-    awsSdkVersion: string;
-    clientId: string;
-  };
-  request: {
-    type: 'customSMSSender';
-    code: string;
-    userAttributes: {
-      phone_number?: string;
-      // other attributes
-    };
-  };
-  response: Record<string, unknown>;
-}
 
 export const handler: CustomSMSSenderTriggerHandler = async (event) => {
   console.log('Received event:', JSON.stringify(event));
@@ -43,24 +21,24 @@ export const handler: CustomSMSSenderTriggerHandler = async (event) => {
     throw new Error('Phone number or encrypted code or userPoolId not found.');
   }
 
-  console.log('Phone number:', phoneNumber);
-  console.log('Encrypted code:', encryptedCode);
-  console.log('userPoolId:', userPoolId);
+  // console.log('Phone number:', phoneNumber);
+  // console.log('Encrypted code:', encryptedCode);
+  // console.log('userPoolId:', userPoolId);
 
-  const decryptCommand = new DecryptCommand({
-    CiphertextBlob: Buffer.from(encryptedCode, 'base64'),
-    EncryptionAlgorithm: 'SYMMETRIC_DEFAULT',
-    EncryptionContext: { UserPoolId: userPoolId }
-  });
+  // const decryptCommand = new DecryptCommand({
+  //   CiphertextBlob: Buffer.from(encryptedCode, 'base64'),
+  //   EncryptionAlgorithm: 'SYMMETRIC_DEFAULT',
+  //   EncryptionContext: { UserPoolId: userPoolId }
+  // });
 
-  const { Plaintext } = await kmsClient.send(decryptCommand);
-  if (!Plaintext) {
-    throw new Error('Failed to decrypt verification code.');
-  }
+  // const { Plaintext } = await kmsClient.send(decryptCommand);
+  // if (!Plaintext) {
+  //   throw new Error('Failed to decrypt verification code.');
+  // }
 
-  const code = Buffer.from(Plaintext).toString('utf-8');
+  // const code = Buffer.from(Plaintext).toString('utf-8');
 
-  const message = `Your verification code is: ${code} __ ${encryptedCode}`;
+  const message = `Your verification code is: Code __ ${encryptedCode}`;
 
   try {
     await smsService.sendSms({
