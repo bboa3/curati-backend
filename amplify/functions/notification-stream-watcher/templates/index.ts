93f9a6837200c5c3e0bd7a78c7e03ead72390f5e
Arlindo Boa <arlindojosboa@gmail.com>
2025-05-21 00:28:03 +0200

    Test

--- amplify/functions/notification-stream-watcher/templates/index.ts
+++ amplify/functions/notification-stream-watcher/templates/index.ts
@@ -1,38 +1,9 @@
-import { Notification, NotificationTemplateKey } from '../../helpers/types/schema';
-import { Message } from '../helpers/types';
-import * as generator from './generator';
+import { Notification } from '../../helpers/types/schema';
 
 interface TemplateInput {
   notification: Notification
 }
 
-const templateMap: Record<NotificationTemplateKey, (input: TemplateInput) => Promise<Message>> = {
-  [NotificationTemplateKey.APPOINTMENT_CONFIRMATION_REQUIRED]: generator.generateAppointmentConfirmationRequiredMessages,
-  [NotificationTemplateKey.APPOINTMENT_CONFIRMED]: generator.generateAppointmentConfirmedMessages,
-  [NotificationTemplateKey.APPOINTMENT_RESCHEDULE_REQUIRED]: generator.generateAppointmentRescheduleRequiredMessages,
-  [NotificationTemplateKey.APPOINTMENT_CANCELLED]: generator.generateAppointmentCancelledMessages,
-  [NotificationTemplateKey.APPOINTMENT_REMINDER]: generator.generateAppointmentReminderMessages,
-  [NotificationTemplateKey.APPOINTMENT_JOIN_READY]: generator.generateAppointmentJoinReadyMessages,
-  [NotificationTemplateKey.PRESCRIPTION_VALIDATION_REQUIRED]: generator.generatePrescriptionValidationRequiredMessages,
-  [NotificationTemplateKey.PRESCRIPTION_STATUS_UPDATED]: generator.generatePrescriptionStatusUpdatedMessages,
-  [NotificationTemplateKey.DELIVERY_ASSIGNMENT_AVAILABLE]: generator.generateDeliveryAssignmentAvailableMessages,
-  [NotificationTemplateKey.DELIVERY_DRIVER_ASSIGNED]: generator.generateDeliveryDriverAssignedMessages,
-  [NotificationTemplateKey.DELIVERY_STATUS_PATIENT_UPDATE]: generator.generateDeliveryStatusPatientUpdateMessages,
-  [NotificationTemplateKey.DELIVERY_TASK_DRIVER_UPDATE]: generator.generateDeliveryTaskDriverUpdateMessages,
-  [NotificationTemplateKey.DELIVERY_EVENT_ADMIN_ALERT]: generator.generateDeliveryEventAdminAlertMessages,
-  [NotificationTemplateKey.MEDICINE_ORDER_CREATED]: generator.generateMedicineOrderCreatedMessages,
-  [NotificationTemplateKey.INVOICE_CREATED]: generator.generateInvoiceCreatedMessages,
-  [NotificationTemplateKey.INVOICE_STATUS_UPDATED]: generator.generateInvoiceStatusUpdatedMessages,
-  [NotificationTemplateKey.CONTRACT_STATUS_PATIENT_UPDATE]: generator.generateContractStatusPatientUpdateMessages,
-  [NotificationTemplateKey.CONTRACT_STATUS_PROFESSIONAL_UPDATE]: generator.generateContractStatusProfessionalUpdateMessages,
-};
-
 export const generateMessages = ({ notification }: TemplateInput) => {
-  const generator = templateMap[notification.templateKey as NotificationTemplateKey];
 
-  if (!generator) {
-    throw new Error(`No template generator found for key: ${notification.templateKey}`);
-  }
-
-  return generator({ notification });
 };
--- amplify/functions/notification-stream-watcher/triggers/post-notification-creation.ts
+++ amplify/functions/notification-stream-watcher/triggers/post-notification-creation.ts
@@ -1,9 +1,5 @@
 import { Logger } from "@aws-lambda-powertools/logger";
-import { unmarshall } from "@aws-sdk/util-dynamodb";
 import type { AttributeValue } from "aws-lambda";
-import { Notification } from '../../helpers/types/schema';
-import { sendMessage } from "../channels";
-import { generateMessages } from "../templates";
 
 interface TriggerInput {
   notificationImage: { [key: string]: AttributeValue; };
@@ -12,16 +8,5 @@ interface TriggerInput {
 }
 
 export const postNotificationCreation = async ({ notificationImage, dbClient, logger }: TriggerInput) => {
-  const notification = unmarshall(notificationImage as any) as Notification;
 
-  const message = await generateMessages({ notification })
-  if (!message) return;
-
-  await sendMessage({
-    message: message,
-    logger: logger,
-    dbClient: dbClient,
-    userId: notification.userId,
-    notificationId: notification.id
-  });
 };
