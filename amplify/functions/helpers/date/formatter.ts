379635976010e506d03d0cc009d5720084ff0c53
Arlindo Boa <arlindojosboa@gmail.com>
2025-05-20 14:07:00 +0200

    IMPROVE Remove unecessary dayjs config

--- amplify/functions/helpers/date/formatter.ts
+++ amplify/functions/helpers/date/formatter.ts
@@ -1,11 +1,9 @@
 import dayjs from 'dayjs';
 import 'dayjs/locale/pt-br';
 import duration from 'dayjs/plugin/duration';
-import localizedFormat from 'dayjs/plugin/localizedFormat';
 import timezone from 'dayjs/plugin/timezone';
 import utc from 'dayjs/plugin/utc';
 
-dayjs.extend(localizedFormat);
 dayjs.extend(utc);
 dayjs.extend(timezone);
 dayjs.extend(duration);
